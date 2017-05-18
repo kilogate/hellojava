93e2fd58f38d1cff795836f0bda8e884419a16bc
kilogate <kilogate@163.com>
2017-05-18 14:24:44 +0800

    添加 tomcat 项目

---
+++ hello-tomcat/src/main/java/com/kilogate/hello/tomcat/test/Test.java
@@ -0,0 +1,13 @@
+package com.kilogate.hello.tomcat.test;
+
+/**
+ * 测试
+ *
+ * @author fengquanwei
+ * @create 2017/5/18 12:40
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello-tomcat");
+    }
+}
