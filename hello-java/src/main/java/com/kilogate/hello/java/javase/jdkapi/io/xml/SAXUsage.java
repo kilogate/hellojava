34538b1b6175876910942c0634060ab6cd08008c
kilogate <kilogate@163.com>
2017-07-20 17:33:08 +0800

    sax

--- hello-java/src/main/java/com/kilogate/hello/java/javase/jdkapi/io/xml/SAXUsage.java
+++ hello-java/src/main/java/com/kilogate/hello/java/javase/jdkapi/io/xml/SAXUsage.java
@@ -1,10 +1,15 @@
 package com.kilogate.hello.java.javase.jdkapi.io.xml;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * SAX 流解析器用法
@@ -13,10 +18,30 @@ import javax.xml.parsers.SAXParserFactory;
  * @create 2017/7/19 15:46
  **/
 public class SAXUsage {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
+        String url = "http://www.w3c.org";
+
+        DefaultHandler defaultHandler = new DefaultHandler() {
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                if (localName.equals("a") && attributes != null) {
+                    for (int i = 0; i < attributes.getLength(); i++) {
+                        String attributesLocalName = attributes.getLocalName(i);
+                        if (attributesLocalName.equals("href")) {
+                            System.out.println(attributes.getValue(i));
+                        }
+                    }
+                }
+            }
+        };
+
         SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        saxParserFactory.setNamespaceAware(true);
+        saxParserFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
         SAXParser saxParser = saxParserFactory.newSAXParser();
 
-
+        InputStream inputStream = new URL(url).openStream();
+        saxParser.parse(inputStream, defaultHandler);
     }
 }
